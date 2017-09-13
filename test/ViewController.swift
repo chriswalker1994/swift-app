c0c1bb1dd14e18cfa110c1966e0dd849e6c4b146
chriswalker1994 <cwalker155@googlemail.com>
2017-09-13 18:56:30 +0300

    My second commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -3,16 +3,27 @@
 //  test
 //
 //  Created by Christopher Walker on 13/09/2017.
-//  Copyright © 2017 Christopher Walker. All rights reserved.
+//  Copyright © 2017 Christopher Walker. All rights reserved.....
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var theLabel: UILabel!
+    var tapCount = 0
+    @IBAction func First(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount >= 4 {
+            theLabel.text = "Avenit Cafea"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
